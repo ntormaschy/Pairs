8f1a7ae5b4aedb888ea1982210b9ddb438cd5c46
ntormaschy <108035998+ntormaschy@users.noreply.github.com>
2022-06-28 21:57:22 -0500

    Added Google Cloud Connection

--- DATA/INIT_STORE.py
+++ DATA/INIT_STORE.py
@@ -1,15 +1,25 @@
 import os
 import json 
 import requests
+import pandas as pd
+import mysql.connector
+
+from mysql.connector.constants import ClientFlag
 
 IS_DEMO = True
 ENV_SETTING = "OANDA_DEMO"
 ACT_SETTING = "OANDA_DEMO_ACT_NUM"
-KEY_PATH = r"..\PRIVATE\KEYS.txt"
+KEY_PATH = r".\PRIVATE\KEYS.txt"
 
 KEY_DICT = json.load(open(KEY_PATH))
 API_KEY = KEY_DICT[ENV_SETTING]
 ACT_NUM = KEY_DICT[ACT_SETTING]
+
+SSL_CA_PATH = r".\PRIVATE\server-ca.pem"
+SSL_CERT_PATH = r".\PRIVATE\client-cert.pem"
+SSL_KEY_PATH = r".\PRIVATE\client-key.pem"
+GCLOUD_IP = KEY_DICT['GOOGLE_CLOUD_SQL_IP']
+GCLOUD_PWD = KEY_DICT['GOOGLE_CLOUD_SQL_PWD']
 
 global REQUEST_HEADERS
 REQUEST_HEADERS = {
@@ -22,6 +32,20 @@ if not IS_DEMO:
 else:
     ACT_URL = "https://api-fxpractice.oanda.com/v3/accounts/"
 
+def connect_cloud_sql(db, pwd, host, ssl_ca, ssl_cert, ssl_key):
+    config = {
+        'database' : db,
+        'user': 'root',
+        'password': pwd,
+        'host': host,
+        'client_flags': [ClientFlag.SSL],
+        'ssl_ca': ssl_ca,
+        'ssl_cert': ssl_cert,
+        'ssl_key': ssl_key
+    }
+    cnxn = mysql.connector.connect(**config)
+    return cnxn
+
 def handle_response(res):
     status_code = int(res.status_code)
 
@@ -33,8 +57,6 @@ def handle_response(res):
         print(res.text)
         return None
     
-
-
 def get_tradeable_pairs(account_num):
     url = ACT_URL + "/{}/instruments".format(str(account_num))
 
@@ -49,7 +71,44 @@ def get_tradeable_pairs(account_num):
     if data == None:
         print("Failed to fetch pairs")
         return None
-    else:
-        print(data)
+    
+    rows = []
+    cols = ['name', 'type', 'displayName', 'displayPrecision', 'marginRate', 'longRate', 'ShortRate']
+    instruments = data['instruments']
+
+    for instrument in instruments:
+        row = [instrument['name'], instrument['type'], instrument['displayName'], instrument['displayPrecision'],
+                instrument['marginRate'], instrument['financing']['longRate'], instrument['financing']['shortRate']]
+        rows.append(row)
+
+    pairs_df = pd.DataFrame(rows)
+    pairs_df.columns = cols
+
+    cnxn = connect_cloud_sql('PAIRS', GCLOUD_PWD, GCLOUD_IP, SSL_CA_PATH, SSL_CERT_PATH, SSL_KEY_PATH)
+    cursor = cnxn.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS TRADEABLE_PAIRS ("
+               "name VARCHAR(64),"
+               "type VARCHAR(64),"
+               "displayName VARCHAR(64),"
+               "displayPrecision INT(2),"
+               "marginRate FLOAT(24),"
+               "longRate FLOAT(24),"
+               "ShortRate FLOAT(24) )")
+    cursor.execute("SELECT * FROM TRADEABLE_PAIRS")
+    out = cursor.fetchall()
+    if len(out) >0:
+        print("Tradeable Pairs Already Created")
+        for row in out[0:5]:
+            print(row)
+    else:  
+        query = ("INSERT INTO TRADEABLE_PAIRS (name, type, displayName, displayPrecision, marginRate, longRate, ShortRate) "
+            "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+
+        cursor.executemany(query, list(pairs_df.values.tolist()))
+        print("Tradeable Pairs Table Created")
+        
+    cnxn.commit() 
+    cnxn.close() 
 
 get_tradeable_pairs(ACT_NUM)
